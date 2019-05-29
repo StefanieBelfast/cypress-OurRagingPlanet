b55dfd2ef26773a7667c4002bf36c4912ecf6e9d
StefanieBelfast <49611105+StefanieBelfast@users.noreply.github.com>
2019-05-29 17:04:41 +0100

    update

--- map.js
+++ map.js
@@ -4,6 +4,10 @@ describe('map', function () {
         .wait(500)
     })
 
+    it('Item', () => {
+        cy.get('[src="https://mo.ev.openindustry.in/fat-orp-deploy/cdn/dist-b035268c81ada6e61b23098d733948f2594491ef/images/leaflet/focal-point/marker-icon.png"]')
+    })
+
     it('countItem', () => {
         cy.get('.leaflet-marker-icon')
     })

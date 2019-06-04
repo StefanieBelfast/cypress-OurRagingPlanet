1d968207da25c053a8bbca60ffbab3d2bac9ab31
StefanieBelfast <49611105+StefanieBelfast@users.noreply.github.com>
2019-06-04 15:02:42 +0100

    update

--- countItem.js
+++ countItem.js
@@ -6,13 +6,29 @@ describe('count items', function () {
 
     it('count items', () => {
       cy.get('.vis-text').should('have.length', 12)
+         })
+        it('count items', () => {
       cy.get('.vis-minor').should('have.length', 19)
+             })
+            it('count items', () => {
       cy.get('.vis-even').should('have.length', 11)
+                 })
+                it('count items', () => {
       cy.get('.vis-odd').should('have.length', 8)
+                     })
+                    it('count items', () => {
       cy.get('.menu').should('have.length',1)
+                       })  
+                        it('count items', () => {
       cy.get('.vis-panel').should('have.length',8)
+                      })       
+                            it('count items', () => {
       cy.get('.vis-major').should('have.length',2)
+                    })             
+                                it('count items', () => {
       cy.get('.leaflet-pane').should('have.length',7)
+              })                       
+                                    it('count items', () => {
       cy.get('.leaflet-marker-pane').should('have.length',1)
       //cy.get('.leaflet-interactive')
   })
@@ -20,16 +36,34 @@ describe('count items', function () {
     it('check items', () => {
         //cy.get('.vis-custom-time')
         cy.get('.panel-page__icon-symbol')
+         })
+        it('count items', () => {
         cy.get('.panel-page__phenomenon-header').contains('Hurricane Storm Surge')
+             })
+            it('count items', () => {
         cy.get('.panel-page__caption-header').contains('Notes')
+                 })
+                it('count items', () => {
         cy.get('[name="play"]').contains('PLAY')//.click()
+                     })
+                    it('count items', () => {
         cy.get('.panel-page__time-header-block').contains('Current Time')//.click()
+                       })  
+                        it('count items', () => {
         //cy.get('.mapdialog-wrapper').contains([id="dialogMenu"])
         ///it stacks because teh webpage is not running 
         cy.get('#clickNewsFeed').contains('NO NEWS')
+                   })          
+                            it('count items', () => {
         cy.get('.vis-foreground')
+                })                 
+                                it('count items', () => {
         cy.get('.btn').contains('HOURS')
+              })                       
+                                    it('count items', () => {
         cy.get('.btn').contains('DAYS')
+           })                               
+                                        it('count items', () => {
         cy.get('#clicktaskwork').contains('NO TASKS')
 
     })
